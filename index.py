6853f453aed73167f0035d4442b5d329fb9e4744
Ernesto Martinez <ernes@perudyptes.cs.pdx.edu>
2019-10-19 14:49:38 -0700

    setUp

--- index.py
+++ index.py
@@ -1,10 +1,51 @@
-print("hello")
+import os
+import numpy
 
 # pick a P = person. ( Host 1 for example)
 # y = best score of P. y = index. X += y 
 # jump to that Index and make it P. 
 # now find biggest score of P(y) that has not been a P yet. 
 # P(y) = index = y. X += y.
-# Repeat  
+# Repeat
+
+matrix = numpy.array([])
+table = numpy.array([])
+
+
+
+# Table set up with all 0's
+def matrix():
+    inst = open("hw1-inst1.txt", "r")
+    f = inst.readline()
+    n = int(f)
+    matrix = [line.split() for line in inst]
+    matrix = numpy.asarray(matrix, dtype=int)
+    #for row in matrix:
+     #   print(row)
+    inst.close()
+    return n
+
+table = numpy.random.randint(1,10, size=(2,5))
+
+def matching(a,b):
+    #if they both hosts or guests, then return 0
+    # else return 1
+    n = matrix()
+    if(a <= (n/2) and b <(n/2)):
+        return 0
+    else:
+        return 1
+
+
+
+
+#print(table)
+print(matching(1,10))
+
+
+
+
+
+
 
 
