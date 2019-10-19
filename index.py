c8ec2d400b5f91f8b1fdd8faa0331f32469b0aab
Ernesto <ernechelsea@pdx.edu>
2019-10-19 11:37:32 -0700

    algorithm

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 print("hello")
+
+# pick a P = person. ( Host 1 for example)
+# y = best score of P. y = index. X += y 
+# jump to that Index and make it P. 
+# now find biggest score of P(y) that has not been a P yet. 
+# P(y) = index = y. X += y.
+# Repeat  
+
+
