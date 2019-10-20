63a1f2d871847c3df017cdd543adaad76732e48e
ernes7 <ernes@pdx.edu>
2019-10-20 00:08:32 -0700

    scoring done

--- index.py
+++ index.py
@@ -1,39 +1,19 @@
 import os
 import numpy
 
-# pick a P = person. ( Host 1 for example)
-# y = best score of P. y = index. X += y 
-# jump to that Index and make it P. 
-# now find biggest score of P(y) that has not been a P yet. 
-# P(y) = index = y. X += y.
-# Repeat
-
-matrix = numpy.array([])
 table = numpy.array([])
-
-
+table = numpy.random.randint(1,10, size=(2,5))
 
 # Table set up with all 0's
 def data():
     inst = open("hw1-inst1.txt", "r")
     f = inst.readline()
     n = int(f)
+    matrix = numpy.array([])
     matrix = [line.split() for line in inst]
     matrix = numpy.asarray(matrix, dtype=int)
     inst.close()
     return matrix
-
-table = numpy.random.randint(1,10, size=(2,5))
-
-def matching(a,b):
-    #if they both hosts or guests, then return 0
-    # else return 1
-    n = 10
-    if(a <= ((n/2)+1) and b <= ((n/2)+1)):
-        return 0
-    else:
-        return 1
-
 
 def scoring(table):
     m = data() #matrix of guest and hosts
@@ -42,22 +22,34 @@ def scoring(table):
     s = 0 #score
     p = 0 #person
 
-    p = table[r][c]
+    print(table)
 
-    #top&bot
-    if (table[r+1][c]):
-        s = s + m[p][table[r+1][c]]
-        s = s + m[table[r+1][c]][p]
+    # Add up scores horizontally
+    # 1<->2, 2<->3, 3<->4 ... etc
+    for r in range(2):
+        for c in range(4):  #make dynamic
+            p = table[r][c]
+            #right
+            if table[r][c+1]:
+                s = s + m[p][table[r][c+1]]
+                s = s + m[table[r][c+1]][p]
+            if p < 5 and table[r][c+1] >= 5:  #make dynamic
+                s = s + 1
 
-    #right
-    if(table[r][c+1]):
-        s = s + m[p][table[r][c+1]]
-        s = s + m[table[r][c+1]][p]
-    
+    # Add up scores vertically
+    # 1<->(n/2)+1, 2<->(n/2)+2, 3<->(n/2)+3 ... etc
+    for r in range(1):
+        for c in range(5): #make dynamic
+            p = table[r][c]
+            if (table[r+1][c]):
+                s = s + m[p][table[r+1][c]]
+                s = s + m[table[r+1][c]][p]
+            if p < 5 and table[r+1][c] >= 5:  #make dynamic
+                s = s + 2
+
     print(s)
 
-
-print(table)
+print(data())
 scoring(table)
 
 
