c0d373089ed117c9f7e23028752c0fb08ac0c1e0
ernes7 <ernes@pdx.edu>
2019-10-19 17:02:46 -0700

    random table

--- index.py
+++ index.py
@@ -14,33 +14,53 @@ table = numpy.array([])
 
 
 # Table set up with all 0's
-def matrix():
+def data():
     inst = open("hw1-inst1.txt", "r")
     f = inst.readline()
     n = int(f)
     matrix = [line.split() for line in inst]
     matrix = numpy.asarray(matrix, dtype=int)
-    #for row in matrix:
-     #   print(row)
     inst.close()
-    return n
+    return matrix
 
 table = numpy.random.randint(1,10, size=(2,5))
 
 def matching(a,b):
     #if they both hosts or guests, then return 0
     # else return 1
-    n = matrix()
-    if(a <= (n/2) and b <(n/2)):
+    n = 10
+    if(a <= ((n/2)+1) and b <= ((n/2)+1)):
         return 0
     else:
         return 1
 
 
+def scoring(table):
+    m = data() #matrix of guest and hosts
+    r = 0 #rows
+    c = 0 #columns
+    s = 0 #score
+    p = 0 #person
+
+    p = table[r][c]
+
+    #top&bot
+    if (table[r+1][c]):
+        s = s + m[p][table[r+1][c]]
+        s = s + m[table[r+1][c]][p]
+
+    #right
+    if(table[r][c+1]):
+        s = s + m[p][table[r][c+1]]
+        s = s + m[table[r][c+1]][p]
+    
+    print(s)
 
 
-#print(table)
-print(matching(1,10))
+print(table)
+scoring(table)
+
+
 
 
 
