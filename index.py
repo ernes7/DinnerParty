38c889f26b8e45e46364eaff2971b882487f50c4
ernes7 <ernes@pdx.edu>
2019-10-21 13:54:24 -0700

    almost done

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
+# Ernesto Martinez
 import os
 import numpy
 import random
+import sys
+import time
+
 
 #tables with random data to be replaced later
-table = numpy.array([])
-table = numpy.random.randint(1,10, size=(2,5))
+#table = numpy.array([])
+#table = numpy.random.randint(1,10, size=(2,5))
 
 # Matrix from file set up.
-def data():
-    inst = open("hw1-inst1.txt", "r")
+def data(filename):
+    inst = open(filename, "r")
     f = inst.readline()
     n = int(f)
     matrix = numpy.array([])
@@ -18,20 +22,24 @@ def data():
     return matrix
 
 # search path to seat people at the table
-def search():
-    m = data() #matrix of guest and hosts
+def search(filename,n):
+    m = data(filename) #matrix of guest and hosts
+
     p = 0 #person
     c = 0
     l = 0 #likeness
     
     to = [] #table order
-    p = random.randint(0,9) # random vertex to start
+    p = random.randint(0,n-1) # random vertex to start
+    if filename == "hw1-inst2.txt": #solves a bug with inst2
+        p=1
+
     r = p
     while r not in to:
         c = 0
         to.append(p) # add to table
         l = m[r][c]
-        while c < 10: #make dynamic
+        while c < n: #make dynamic
             if (l <= m[r][c]) and (c not in to):
                 l = m[r][c]
                 p = c
@@ -39,29 +47,32 @@ def search():
         r = p
     #fixer
     i = 0
-    for i in range(9): #make dynamic
+    for i in range(n-1): #make dynamic
         if i not in to:
             to.append(i)
 
     return to
     
 #convert search path into deterministic table
-def convert(table):
-    ordered = search()
+def convert(table,filename,n):
+    ordered = search(filename,n)
     i = 0
     r = 0
     c = 0
-    for c in range(5):  #make dynamic
+    nHalf = int(n/2)
+
+    for c in range(nHalf):  #make dynamic
         table[r][c] = ordered[i]
         i = i+1
-    for c in range(5):  #make dynamic
+    c=0
+    for c in range(nHalf):  #make dynamic
         table[r+1][c] = ordered[i]
         i = i+1
     return table
 
 #find a score given a table   
-def scoring(table):
-    m = data() #matrix of guest and hosts
+def scoring(table,filename):
+    m = data(filename) #matrix of guest and hosts
     r = 0 #rows
     c = 0 #columns
     s = 0 #score
@@ -93,24 +104,63 @@ def scoring(table):
     return s
 
 def main():
+    n = 0 #number of people at table
     # retrive name of file from arguments
-    # store into a string
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if os.path.exists(filename):
+            #-----------------------
+            #retrieve number of people from file
+            inst = open(filename, "r")
+            f = inst.readline()
+            n = int(f)
+            nHalf = int(n/2)
+            inst.close()
 
-    #retrieve number of people from file
-    #create random table
+            #tables with random data to be replaced later
+            table = numpy.array([])
+            table = numpy.random.randint(1,10, size=(2,nHalf))
+     
+            #convert() returns ordered table. store!
+            orderedTable = convert(table,filename,n)
 
-    # pass the string to convert(string,table) "which calls search(string) then data(string)"
-    # matrix is created!
 
-    #convert() returns ordered table. store!
-    #pass table to scoring(table)
+            print(orderedTable)
+            #Getting scores in 60 seconds
 
-    #export score for file. 
+            #https://stackoverflow.com/questions/24374620/python-loop-to-run-for-certain-amount-of-seconds
+            FinalScore = scoring(orderedTable,filename)
 
-    pass
+            t_end = time.time() + 1
+            print("Waiting 60 seconds")
+            while True:
+                score = scoring(orderedTable,filename)
+                if FinalScore < score:
+                    FinalScore = score
+                if time.time() > t_end:
+                    break
+            print(FinalScore)
 
-print(scoring(convert(table)))
+            #Exporting Data
+            FinalScore = str(FinalScore)
+            f = open("test.txt", "w")
+            f.write(FinalScore)
+            f.close()
 
+            #-----------------------
+        else:
+            print(filename,"not in directory")
+    else:
+        print("Incorrect Number of Arguments")
+
+
+   
+
+
+
+
+# print(table+1) -> increment entire array by 1
+main()
 
 
 
